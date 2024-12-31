493028fb11b672e3db879539d6ef7497ee2e740a
Geetika Akki <geetika.akki@gmail.com>
2024-12-31 10:07:59 +0530

    Retrieving Employee Details

--- src/main/java/net/project/ems/service/EmployeeService.java
+++ src/main/java/net/project/ems/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package net.project.ems.service;
 
+import java.util.List;
+
 import net.project.ems.dto.EmployeeDto;
 
 public interface EmployeeService {
  EmployeeDto createEmployee(EmployeeDto employeeDto);
- 
+ EmployeeDto getEmployeeById(Long employeeId);
+ List<EmployeeDto> getAllEmployees();
 }
--- src/main/java/net/project/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/project/ems/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,14 @@
 package net.project.ems.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import net.project.ems.dto.EmployeeDto;
 import net.project.ems.entity.Employee;
+import net.project.ems.exception.ResourceNotFoundException;
 import net.project.ems.mapper.EmployeeMapper;
 import net.project.ems.repository.EmployeeRepository;
 import net.project.ems.service.EmployeeService;
@@ -17,6 +21,18 @@ public class EmployeeServiceImpl implements EmployeeService {
 		Employee employee=EmployeeMapper.mapToEmployee(employeeDto);
 		Employee savedEmployee=employeeRepository.save(employee);
 		return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+	}
+	@Override
+	public EmployeeDto getEmployeeById(Long employeeId) {
+		Employee employee=employeeRepository.findById(employeeId).orElseThrow(()->new ResourceNotFoundException("Employee doesn't exist with the given Id "+ employeeId));
+		
+		return EmployeeMapper.mapToEmployeeDto(employee);
+	}
+	@Override
+	public List<EmployeeDto>getAllEmployees(){
+		List<Employee> employees= employeeRepository.findAll();
+		return employees.stream().map((employee)->EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
+		
 	}
 	
 
